0da439f81bb136904a2fb977a79f1917d3da1dfc
Philippe <p.joseph@simplytest.de>
2024-12-04 10:00:27 +0100

    Projektdateien hinzufügen.

---
+++ demoWinAppDriverPOM/MainApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoWinAppDriverPOM
+{
+    public class MainApp
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
